4b64d2316ef62d4deddea7d4115176cb3a2834e3
Esther-Sonia <esthersonia21@gmail.com>
2025-04-24 22:43:11 +0300

    initial commit

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const jsonServer = require('json-server');
+const cors = require('cors');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+// Set up CORS for cross-origin requests
+server.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+server.use(middlewares);
+server.use(router);
+
+module.exports = server;
